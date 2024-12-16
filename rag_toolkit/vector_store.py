0cc932da6657f29c7e64fecc705c255fcf0eae65
youssef-yasser-ali <yyasser849@gmail.com>
2024-12-16 08:11:42 +0200

    adding create vector store

--- rag_toolkit/vector_store.py
+++ rag_toolkit/vector_store.py
@@ -1,6 +1,10 @@
 from langchain_community.vectorstores import Chroma
 
 
-def create_vector_store_retriever(documents, embeddings_model):
+def create_vector_store(documents,embeddings_model):
+    return Chroma.from_documents(documents=documents, embedding=embeddings_model)
+
+
+def create_vector_store_retriever(documents, embeddings_model,k=2):
     vectorstore = Chroma.from_documents(documents=documents, embedding=embeddings_model)
-    return vectorstore.as_retriever(search_kwargs={"k": 2})
+    return vectorstore.as_retriever(search_kwargs={"k": k})

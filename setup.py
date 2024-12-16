0fec498bb9d4dba3f019e173dedb9b360d97f913
youssef-yasser-ali <yyasser849@gmail.com>
2024-12-16 05:02:46 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rag_toolkit",  
-    version="0.1.0", 
-    packages=find_packages(),
-    description="A toolkit for RAG systems",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author="Youssef Yasser",
+    name="rag-toolkit",
+    version="0.1.0",
+    author="Your Name",
     author_email="yyasser849@gmail.com",
-    # url="https://github.com/youssefyasserali/rag-toolkit",
+    description="A library for building Retrieval-Augmented Generation pipelines.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/youssef-yasser-ali/rag-toolkit",
+    packages=find_packages(),
+    install_requires=[
+        "langchain==0.3.7",
+        "langchain-community==0.3.5",
+        "langsmith==0.1.140",
+        "chromadb==0.5.20",
+        "langchain-google-genai==2.0.4",
+        "pydantic==2.9.2",
+        "pypdf==5.1.0",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7', 
-    install_requires=[
-        "langchain>=0.3.7", 
-        "langchain-community>=0.3.5", 
-        "langchain-core>=0.3.20" ,
-        "chromadb>=0.5.20"
-    ],
+    python_requires=">=3.7",
 )
